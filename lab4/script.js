0d3e0d74a612b583345664bab8533d66fcccf1e8
jasser boulila <jasserboulila@proton.me>
2026-03-26 18:22:12 -0400

    finishied lab 4

---
+++ lab4/script.js
@@ -0,0 +1,32 @@
+// created three variables for the welcome form and the welcome message
+const welcomeForm = document.getElementById('welcomeForm');
+const nameInput = document.getElementById('nameInput');
+const welcomeMessage = document.getElementById('welcomeMessage');
+
+welcomeForm.addEventListener('submit', function(event) {
+    // Prevent the page from refresh
+    event.preventDefault();
+    
+    // Get user's name
+    const userName = nameInput.value;
+
+    // validate the user name if it empty show enter your name and break the function
+    if(!userName){
+        
+        welcomeMessage.style.color = 'red';
+        welcomeMessage.textContent = 'enter your name';
+        return;
+    }
+    // Display welcome message
+    welcomeMessage.textContent = `Welcome, ${userName}!`;
+});
+
+
+
+// function for the light/dark mode toggle
+const toggleDarkMode = ()=>{
+
+    const body = document.body;
+    // toggle dark mode
+    body.classList.toggle('dark-mode');
+}
